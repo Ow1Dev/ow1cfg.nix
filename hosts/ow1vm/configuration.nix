1d8a0c9d4ea9fc89e3e49fb0fbc26c9b097471db
Ow1Dev <31339803+Ow1Dev@users.noreply.github.com>
2024-11-18 18:58:56 +0100

    feat: some small changes

--- hosts/ow1vm/configuration.nix
+++ hosts/ow1vm/configuration.nix
@@ -83,7 +83,16 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    settings.PermitRootLogin = "no";
+    allowSFTP = true;
+  };
+
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
