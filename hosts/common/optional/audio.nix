426b52f1f58754a429bb1a6ebc9f01f82c356820
Ow1Dev <31339803+Ow1Dev@users.noreply.github.com>
2025-03-26 16:54:48 +0100

    feat: adding a new mashine and some service

---
+++ hosts/common/optional/audio.nix
@@ -0,0 +1,20 @@
+{ pkgs, ... }:
+{
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    wireplumber.enable = true;
+    jack.enable = true;
+  };
+
+  environment.systemPackages = builtins.attrValues {
+    inherit (pkgs)
+      playerctl # cli utility and lib for controlling media players
+      # pamixer # cli pulseaudio sound mixer
+      ;
+  };
+}
