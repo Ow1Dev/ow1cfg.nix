a77309813b5d53f1d788a450100f163cfd737722
Ow1Dev <31339803+Ow1Dev@users.noreply.github.com>
2025-10-10 18:21:33 +0200

    feat: adding icon and cursor theme

--- home/desktop/gtk.nix
+++ home/desktop/gtk.nix
@@ -1,8 +1,17 @@
-{
-  gtk = {
+{ pkgs, ... }: {
+gtk = {
     enable = true;
     gtk3.extraConfig = {gtk-application-prefer-dark-theme = 1;};
     gtk4.extraConfig = {gtk-application-prefer-dark-theme = 1;};
+    iconTheme = {
+      name = "Nordzy";
+      package = pkgs.nordzy-icon-theme;
+    };
+    cursorTheme = {
+      name = "Nordzy-cursors";
+      package = pkgs.nordzy-cursor-theme;
+      size = 32;
+    };
   };
 
   dconf.settings = {
